1d0ea31b3a0dfe42f553fff6ba4fc04ecb1864e3
gobytraindev <jarlrobin@hotmail.se>
2025-08-21 13:04:38 +0200

    Add Next.js boilerplate files

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Welcome to Gobytrain</h1>
+      <p>Your train journey starts here.</p>
+    </div>
+  );
+}
